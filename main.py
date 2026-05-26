d468c517dfb2fdcabae32d97fdb53c23bba791ee
nurbolatkayrbolatov1-arch <nurbolatkayrbolatov1@gmail.com>
2026-05-26 12:46:10 +0500

    main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_csv_data():
         return
 
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8-sig') as f:
             reader = csv.DictReader(f)
             for row in reader:
                 lat = float(row['latitude'])
